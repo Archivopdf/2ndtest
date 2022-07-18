dd5a94dd745588ab53d4696d61eecd3dc6a16696
Archivopdf <toni.diaz.gar77@gmail.com>
2022-07-18 18:09:15 +0200

    function

---
+++ index.js
@@ -0,0 +1,18 @@
+function calculadoraSuma(op1, op2) {
+    resultado = op1 + op2;
+    return resultado
+}
+function calculadoraSubs(op1, op2) {
+    resultado = op1 - op2;
+    return resultado
+}
+function calculadoraMult(op1, op2) {
+    resultado = op1 * op2;
+    return resultado
+}
+function calculadoraDiv(op1, op2) {
+    resultado = op1 / op2;
+    return resultado
+}
+
+module.exports = { calculadoraSuma, calculadoraSubs, calculadoraDiv, calculadoraMult } 
